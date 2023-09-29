a9b5d08ca0359daccd9c4cc13ab447d77f37a222
Facundo Carollo <65639107+FacundoCarollo@users.noreply.github.com>
2023-09-29 20:51:07 -0300

    Incio proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { ModuloPrincipalRoutingModule } from './modulo-principal/modulo-principal-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MaterialModule,
+    ModuloPrincipalRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
