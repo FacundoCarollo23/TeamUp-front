1b1fd57d737895abfd81b35ea00b3bb0c07f7f7e
Antonella Duca <antonelladuca@gmail.com>
2023-10-26 19:40:43 -0300

    working on the hero

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
-
+export class HeroComponent implements OnInit{   
+  constructor(){}
+  ngOnInit(): void {
+  
+  }
+  
 }
