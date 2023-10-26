e9e50fabd77164c0adf127c0950d6696106c4605
Antonella Duca <antonelladuca@gmail.com>
2023-10-26 18:33:06 -0300

    hero component  added

--- src/app/modulo-principal/modulo-principal.module.ts
+++ src/app/modulo-principal/modulo-principal.module.ts
@@ -3,14 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from "../shared/nav-bar/nav-bar.component";
 import { MaterialModule } from "../material.module";
 import { TitleComponent } from "../shared/title/title.component";
+import { HeroComponent } from "../shared/hero/hero.component";
 
 
 @NgModule({
     declarations: [
     HomeComponent,
     NavBarComponent,
-    TitleComponent
-
+    TitleComponent,
+    HeroComponent
   ],
     imports: [
       MaterialModule
