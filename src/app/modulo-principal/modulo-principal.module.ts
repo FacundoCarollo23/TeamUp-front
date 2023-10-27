a89ba6485fa0ee4811565a2f8f6ad78c7b0f3814
Facundo Carollo <65639107+FacundoCarollo@users.noreply.github.com>
2023-10-26 22:16:00 -0300

    FC - 2023-10-26 - Card-Eventos

--- src/app/modulo-principal/modulo-principal.module.ts
+++ src/app/modulo-principal/modulo-principal.module.ts
@@ -3,13 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from "../shared/nav-bar/nav-bar.component";
 import { MaterialModule } from "../material.module";
 import { TitleComponent } from "../shared/title/title.component";
+import { CardEventsComponent } from "../shared/cards/card-events/card-events.component";
 
 
 @NgModule({
     declarations: [
     HomeComponent,
     NavBarComponent,
-    TitleComponent
+    TitleComponent,
+    CardEventsComponent,
 
   ],
     imports: [
