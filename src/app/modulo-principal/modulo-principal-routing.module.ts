a3bd847334a290ffad670d26c5711f93aa555fd7
Antonella Duca <antonelladuca@gmail.com>
2023-11-01 20:07:15 -0300

    working on events component

--- src/app/modulo-principal/modulo-principal-routing.module.ts
+++ src/app/modulo-principal/modulo-principal-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
+import { EventsComponent } from "./components/events/events.component";
 
 
 const routes : Routes = [
@@ -10,6 +11,9 @@ const routes : Routes = [
     {
         path: "home", component: HomeComponent  
     },
+    {
+        path: "events", component: EventsComponent
+    }
 ]
 
 @NgModule({
