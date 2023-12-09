b2eb594e9ccb46c0e88db0e526ab90c42b8aa007
Antonella Duca <antonelladuca@gmail.com>
2023-12-09 15:45:40 -0300

    AD 9-12 Order Fecha Dashboard Events +EventView

--- src/app/modulo-principal/components/dashboard-events/dashboard-events.component.ts
+++ src/app/modulo-principal/components/dashboard-events/dashboard-events.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EventService } from 'src/app/api/services';
 import { MatDialog } from '@angular/material/dialog';
+import {Sort, MatSortModule} from '@angular/material/sort';
 
 @Component({
   selector: 'app-dashboard-events',
@@ -12,9 +13,49 @@ export class DashboardEventsComponent implements OnInit {
   public page!: number;
   userJoinedEventsList: any[] = [];
   userLogueado: any = 0;
-  
 
-  constructor(private eventService: EventService, private dialog: MatDialog) {}
+  //ordenamiento Eventos creados por mí  
+  sortData(sort: Sort) {
+    const data = this.userEventsList.slice();
+    if (!sort.active || sort.direction === '') {
+      this.userEventsList = data;
+      return;
+    }
+
+    this.userEventsList = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'fecha':
+          return compare(a.fecha, b.fecha, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  //ordenamiento Mis próximos eventos
+  sortDataNextEvents(sort: Sort) {
+    const data = this.userJoinedEventsList.slice();
+    if (!sort.active || sort.direction === '') {
+      this.userJoinedEventsList = data;
+      return;
+    }
+
+    this.userJoinedEventsList = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'fecha':
+          return compare(a.fecha, b.fecha, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
+  constructor(private eventService: EventService, private dialog: MatDialog) {
+    this.userEventsList = this.userEventsList.slice();
+    this.userJoinedEventsList = this.userEventsList.slice();
+  }
 
   ngOnInit(): void {
     //Obtengo y guardo el Id del usuario Logueado para filtrar por el mismo
@@ -99,5 +140,8 @@ export class DashboardEventsComponent implements OnInit {
     // Implementar la lógica para que el usuario "se baje" del evento
     // Puedes llamar al servicio correspondiente o realizar la lógica necesaria.
   }
-  
+}
+
+function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
