a1812b7ef554d5233ea45c221d9bc51a0648f302
first commit <gonzalogatius@gmail.com>
2023-12-10 15:54:09 -0300

    gg 10/12/23 volver atras en 404 not found

--- src/app/modulo-principal/components/no-page/no-page.component.ts
+++ src/app/modulo-principal/components/no-page/no-page.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-no-page',
   templateUrl: './no-page.component.html',
   styleUrls: ['./no-page.component.css']
 })
+
 export class NoPageComponent {
 
+  constructor(
+    private location: Location,
+    ){}
+
+  volverAtras() {
+    this.location.back();
+  }
 }
